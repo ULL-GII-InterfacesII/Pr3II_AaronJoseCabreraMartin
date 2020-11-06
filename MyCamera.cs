cedce40287686dd40ce555249bd117fb921f5f00
AaronJoseCabreraMartin <alu0101101019@ull.edu.es>
2020-11-06 15:16:46 +0000

    MovimientoJugador+Camara3persona

---
+++ MyCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+    private Transform tf_;
+    private GameObject player_;
+    public float velocity_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf_ = GetComponent<Transform>();
+        player_ = GameObject.FindWithTag("Player");
+        velocity_ = 15F;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 positionJugador = player_.transform.position;
+        if (Input.GetKey("up"))
+        {
+            tf_.Translate(velocity_ * Vector3.forward * Time.deltaTime);
+        } else if (Input.GetKey("down"))
+        {
+            tf_.Translate(velocity_ * Vector3.back * Time.deltaTime);
+        } else if (Input.GetKey("left"))
+        {
+            tf_.RotateAround(positionJugador, Vector3.up, -velocity_*30 * Time.deltaTime);
+        } else if (Input.GetKey("right"))
+        {
+            tf_.RotateAround(positionJugador, Vector3.up, velocity_ * 30 * Time.deltaTime);
+        }
+    }
+}
---
+++ MyCharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class MyCharacterController : MonoBehaviour
+{
+
+    private Transform tf_;
+    public float velocity_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf_ = GetComponent<Transform>();
+        velocity_ = 15F;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            tf_.Translate(velocity_ * Vector3.forward * Time.deltaTime);
+        }else if (Input.GetKey("down"))
+        {
+            tf_.Translate(velocity_ * Vector3.back * Time.deltaTime);
+        }else if (Input.GetKey("left"))
+        {
+            tf_.Rotate(0, -30*velocity_  * Time.deltaTime, 0);
+        }else if (Input.GetKey("right"))
+        {
+            tf_.Rotate(0, 30*velocity_ *  Time.deltaTime, 0);
+        }
+    }
+}
